97e52d37d1e6ce44c85875a3d45f7218f1a243be
Bujlove <130996686+Bujlove@users.noreply.github.com>
2025-04-17 23:20:41 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,22 +2,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Interactive Buttons Loaded!");
 
-    // Обработчик для зеленой кнопки
+    // Обработчики событий для кнопок
     document.getElementById("button1").addEventListener("click", () => {
-        window.location.href = "https://t.me/+O5kPFXq94wVhZjEy"; // Замените ссылку на нужный URL
+        alert("У меня проблемы на работе!");
     });
 
-    // Обработчик для второй кнопки
     document.getElementById("button2").addEventListener("click", () => {
         alert("Меня уволили!");
     });
 
-    // Обработчик для третьей кнопки
     document.getElementById("button3").addEventListener("click", () => {
         alert("Хочу уволиться!");
     });
 
-    // Обработчик для четвертой кнопки
     document.getElementById("button4").addEventListener("click", () => {
         alert("Меня пытаются уволить!");
     });
